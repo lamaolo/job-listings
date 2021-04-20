6a0318cf4aac936f50a396cdf65273442f329bfc
Lucero Amaolo <michilnx@gmail.com>
2021-04-20 14:33:43 -0300

    version desktop finalizada

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,28 +12,32 @@ const Card = ({
 }) => {
   return (
     <article className={isFeatured ? 'Card is-featured' : 'Card'}>
-      <figure className="Card-picture">
-        <img
-          src={require(`../static/images/${companyLogo}`).default}
-          alt={companyName}
-        />
-      </figure>
-      <header className="Card-header">
-        <p>{companyName}</p>
-        <div className="Card-status">
-          {isNew && <span className="status-new">new!</span>}
-          {isFeatured && <span className="status-featured">featured</span>}
-        </div>
-      </header>
-      <section className="Card-info">
-        <strong>{jobTitle}</strong>
-        <div className="Card-info-details">
-          {jobDetails.map((detail) => (
-            <p>{detail}</p>
-          ))}
-        </div>
-      </section>
-      <span className="separator"></span>
+      <div className="Card-content">
+        <figure className="Card-picture">
+          <img
+            src={require(`../static/images/${companyLogo}`).default}
+            alt={companyName}
+          />
+        </figure>
+        <section className="details-container">
+          <header className="Card-header">
+            <p>{companyName}</p>
+            <div className="Card-status">
+              {isNew && <span className="status-new">new!</span>}
+              {isFeatured && <span className="status-featured">featured</span>}
+            </div>
+          </header>
+          <section className="Card-info">
+            <strong>{jobTitle}</strong>
+            <div className="Card-info-details">
+              {jobDetails.map((detail) => (
+                <p>{detail}</p>
+              ))}
+            </div>
+          </section>
+        </section>
+      </div>
+
       <section className="Card-tags">
         {tags.map((tag) => (
           <button onClick={() => handleSetFilter(tag)}>{tag}</button>
